a7027b6530f4cf73f099b44d5c1014709ad6f1e1
Aaron <aaronde314@gmail.com>
2018-02-25 23:33:01 -0500

    Added a map builder, to build a box2D world from a tiled map

---
+++ core/src/com/ex/plat/handlers/MapBodyBuilder.java
@@ -0,0 +1,91 @@
+package com.ex.plat.handlers;
+
+import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.CircleMapObject;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+import static com.ex.plat.Constants.PPM;
+
+public class MapBodyBuilder {
+
+    public static Array<Body> buildShapes(Map map, World world, String layer) {
+        MapObjects objects = map.getLayers().get(layer).getObjects();
+
+        Array<Body> bodies = new Array<Body>();
+
+        for(MapObject object : objects) {
+
+            if (object instanceof TextureMapObject) {
+                continue;
+            }
+
+            Shape shape;
+
+            if (object instanceof RectangleMapObject) {
+                shape = getRectangle((RectangleMapObject) object);
+            } else if (object instanceof PolygonMapObject) {
+                shape = getPolygon((PolygonMapObject) object);
+            } else if (object instanceof CircleMapObject) {
+                shape = getCircle((CircleMapObject) object);
+            } else {
+                continue;
+            }
+
+            BodyDef bDef = new BodyDef();
+            bDef.type = BodyDef.BodyType.StaticBody;
+            Body body = world.createBody(bDef);
+            body.createFixture(shape, 1);
+
+            bodies.add(body);
+
+            shape.dispose();
+        }
+        return bodies;
+    }
+
+    private static PolygonShape getRectangle(RectangleMapObject rectangleObject) {
+        Rectangle rectangle = rectangleObject.getRectangle();
+        PolygonShape polygon = new PolygonShape();
+        Vector2 size = new Vector2((rectangle.x + rectangle.width * 0.5f) / PPM,
+        (rectangle.y + rectangle.height * 0.5f) / PPM);
+        polygon.setAsBox(rectangle.width * 0.5f / PPM, rectangle.height * 0.5f / PPM, size, 0.0f);
+
+        return polygon;
+    }
+
+    private static CircleShape getCircle(CircleMapObject circleObject) {
+        Circle circle = circleObject.getCircle();
+        CircleShape circleShape = new CircleShape();
+        circleShape.setRadius(circle.radius / PPM);
+        circleShape.setPosition(new Vector2(circle.x / PPM, circle.y / PPM));
+        return circleShape;
+    }
+
+    private static PolygonShape getPolygon(PolygonMapObject polygonObject) {
+        PolygonShape polygon = new PolygonShape();
+        float[] vertices = polygonObject.getPolygon().getTransformedVertices();
+
+        float[] worldVertices = new float[vertices.length];
+
+        for (int i = 0; i < vertices.length; i++) {
+            worldVertices[i] = vertices[i] / PPM;
+        }
+
+        polygon.set(worldVertices);
+        return polygon;
+    }
+}
